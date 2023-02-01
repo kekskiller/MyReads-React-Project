966c312e299506d91d75246740a7f54d3b2cdba7
kekskiller <kekskiller@email.com>
2023-02-01 15:41:29 +0100

    implement content of shelves showing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import "../css/App.css";
 
 import { useState, useEffect } from "react";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
 import * as BooksAPI from '../utils/BooksAPI'
 import { Search } from "./Search";
@@ -23,16 +23,24 @@ function App() {
 
   return (
     <div className="app">
+      <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
       <Routes>
         <Route 
           path="/"
-          element={<Shelf />}
+          element={<Shelf allBooks={allBooks}/>}
         />
         <Route
           path="/search"
           element={<Search /> }
         />
       </Routes>
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link> 
+      </div>
     </div>
   );
 }
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,5 +1,30 @@
 import { BooksList } from "./BooksList";
 
-export const Shelf = () => {
-    
+export const Shelf = ({allBooks}) => {
+    const shelfNames= {
+        currentlyReading: "Currently Reading", 
+        wantToRead: "Want To Read", 
+        read: "Read"
+    }    
+
+    const displayShelves = Object.keys(shelfNames).map((shelf) => 
+        { return(
+            <div>
+                <h2 className="bookshelf-title">{shelfNames.shelf}</h2>
+                <BooksList 
+                    className="bookshelf-books" 
+                    list={allBooks.filter(books =>  books.shelf === shelf)} 
+                    key = {shelf}                        
+                />   
+            </div>
+        )}        
+    )
+
+    return(        
+        <div className="list-books-content">           
+            <div className="bookshelf">
+                {displayShelves}                    
+            </div>
+        </div>        
+    )
 }
